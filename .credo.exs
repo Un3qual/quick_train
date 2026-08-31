f127480a271e12b4db412f384cef2f20ddfaa673
Ryan <Un3qual@users.noreply.github.com>
2026-08-31 15:08:47 -0700

    scope Ash unaliased module exemption

--- .credo.exs
+++ .credo.exs
@@ -1,15 +1,9 @@
-ash_fully_qualified_files = [
-  "lib/quick_train/assets/asset/cleanup.ex",
-  "lib/quick_train/datasets/dataset_import/cleanup.ex",
-  "lib/quick_train/datasets/dataset_item_revision/actions/put.ex",
-  "lib/quick_train/datasets/dataset_schema_version/actions/create_draft.ex",
-  "lib/quick_train/enterprise_identity/directory_user/validations/identity_scope.ex"
-]
+Code.require_file("credo_checks/unaliased_module_use.ex", __DIR__)
 
 checks =
   Enum.map(ExSlop.checks(), fn
-    ExSlop.Check.Readability.UnaliasedModuleUse = check ->
-      {check, files: %{excluded: ash_fully_qualified_files}}
+    ExSlop.Check.Readability.UnaliasedModuleUse ->
+      {QuickTrain.CredoChecks.UnaliasedModuleUse, []}
 
     check ->
       {check, []}
